a01aedf63009063e27ede854fcf70b1d1523144b
klh6157 <Scytrius@gmail.com>
2023-02-14 12:55:28 -0500

    It finally worked

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div class="characterCard">
+  <h2>Fate/Stay Night: Unlimited Blade Works</h2>
+  <link rel="stylesheet" href="./App.css"></link>
+  <button class="btn" onclick="hideText()">Details</button>
+  <button class="add">Add Stuff</button>
+  <button class="delete">Delete Stuff</button>
+  <button class="color">Color Stuff</button>
+  <button class="heading">Heading Stuff</button>
+  <img src="https://pm1.narvii.com/5770/c91ff789cb31cff4d78b367fdc4f933ccde1a26d_hq.jpg" alt="https://pm1.narvii.com/5770/c91ff789cb31cff4d78b367fdc4f933ccde1a26d_hq.jpg" class="center"></img>
+       <div class="information">
+         <p>Shiro Emiya is a servant of the fate and the main character of the anime. The anime is about a holy grail war where masters summons servants to fight in battle. There are usually seven masters per holy grail war that fight in battle. Winning the holy grail war allows the winner to get one wish granted.</p>
+  </div>
+</div>
   );
 }
 
